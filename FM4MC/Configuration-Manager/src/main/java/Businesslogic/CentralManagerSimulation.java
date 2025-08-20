4a05260f93a435ff54e74f36347becc63879dd92
Paul Wolfart <44647630+saso0@users.noreply.github.com>
2025-08-20 16:54:49 +0200

    add new main to Configuration-Manager Startup

---
+++ FM4MC/Configuration-Manager/src/main/java/Businesslogic/CentralManagerSimulation.java
@@ -0,0 +1,74 @@
+package Businesslogic;
+
+import ConfigurationSerializer.ConfigurationSerializer;
+import FeatureModelMerger.HardwareSensitiveFeatureModelMerger;
+import FeatureModelMerger.Structures.AvailableEdgeHardware;
+import FeatureModelReader.FeatureModelReader;
+import FeatureModelReader.InvalidFeatureModelRelationException;
+import IO.impl.LshwClass;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+
+public class CentralManagerSimulation {
+    private final Logger _Logger;
+
+
+    public CentralManagerSimulation(Logger logger) {
+        _Logger = logger;
+    }
+
+
+    public void startOnlinePhase(String configurationsPath, String fmFilePath, String edgeIndex) {
+        var edgeInformation = getEdgeInformation(edgeIndex);
+        if (edgeInformation == null) {
+            _Logger.error(edgeIndex + " is not a valid Edge Index");
+        } else {
+            var fmReader = new FeatureModelReader(_Logger);
+            var fmSerializer = new ConfigurationSerializer(_Logger);
+            try {
+                var featureModelWithConfigurations = fmSerializer.loadConfigurations(fmReader.readFeatureModelJson(new File(fmFilePath)), configurationsPath);
+
+                var merger = new HardwareSensitiveFeatureModelMerger(_Logger);
+                var graph = merger.startForTesting(featureModelWithConfigurations, edgeInformation, 14);
+
+            } catch (InvalidFeatureModelRelationException e) {
+                _Logger.fatal(e);
+            }
+        }
+    }
+
+    private AvailableEdgeHardware getEdgeInformation(String edgeIndex) {
+        AvailableEdgeHardware edgeInformation = null;
+        switch (edgeIndex) {
+            case "1" -> {
+                edgeInformation = new AvailableEdgeHardware(2);
+                edgeInformation.edgeHardware.put(LshwClass.DISPLAY, 1);
+                edgeInformation.edgeHardware.put(LshwClass.PROCESSOR, 3);
+                edgeInformation.edgeHardware.put(LshwClass.MEMORY, 2);
+            }
+            case "2" -> {
+                edgeInformation = new AvailableEdgeHardware(4);
+                edgeInformation.edgeHardware.put(LshwClass.DISPLAY, 10);
+                edgeInformation.edgeHardware.put(LshwClass.PROCESSOR, 6);
+                edgeInformation.edgeHardware.put(LshwClass.MEMORY, 5);
+            }
+            case "3" -> {
+                edgeInformation = new AvailableEdgeHardware(6);
+                edgeInformation.edgeHardware.put(LshwClass.DISPLAY, 10);
+                edgeInformation.edgeHardware.put(LshwClass.PROCESSOR, 6);
+                edgeInformation.edgeHardware.put(LshwClass.MEMORY, 5);
+            }
+            case "4" -> {
+                edgeInformation = new AvailableEdgeHardware(8);
+                edgeInformation.edgeHardware.put(LshwClass.DISPLAY, 10);
+                edgeInformation.edgeHardware.put(LshwClass.PROCESSOR, 9);
+                edgeInformation.edgeHardware.put(LshwClass.MEMORY, 8);
+            }
+            case "5" -> {
+                edgeInformation = new AvailableEdgeHardware(10);
+            }
+        }
+        return edgeInformation;
+    }
+}
--- FM4MC/Configuration-Manager/src/main/java/Startup.java
+++ FM4MC/Configuration-Manager/src/main/java/Startup.java
@@ -1,5 +1,9 @@
+import BusinessLogic.FeatureModelPreProcessor;
 import Businesslogic.CentralManager;
+import Businesslogic.CentralManagerSimulation;
 import Console.ThreadSafeConsoleHandler;
+import FeatureModelMerger.Structures.AvailableEdgeHardware;
+import IO.impl.LshwClass;
 import Monitoring.Event.Logging.impl.LogInformation;
 import Network.Connection.impl.ConnectionInformation;
 import Network.Facade.impl.ConnectionFacade;
@@ -8,6 +12,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 /**
  * Entry point for the configuration manager. Sets up logging, networking
@@ -17,12 +22,59 @@ public class Startup {
     private static final Logger _Logger = LogManager.getLogger("executionLog");
 
     /**
+     * Application entry point.
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        var logParameters = new LogInformation(args);
+        var logManager = new MyLogManager(logParameters);
+        logManager.start();
+
+        var arguments = getTestbedParameters(args);
+        var processLogic = new CentralManagerSimulation(_Logger);
+
+
+        if (!arguments.containsKey("configurations")) {
+            _Logger.error("Missing argument for configuration input path");
+        } else if (!arguments.containsKey("fmFile")) {
+            _Logger.error("Missing argument for Feature Model input path");
+        } else if (!arguments.containsKey("edgeIndex")) {
+            _Logger.error("Missing Edge Index: 1 for tiny, 2 for small, 3 for medium, 4 for big, 5 for huge");
+        } else {
+            processLogic.startOnlinePhase(arguments.get("configurations"), arguments.get("fmFile"), arguments.get("edgeIndex"));
+        }
+    }
+
+
+
+    /**
+     * Parses the command line arguments into a key/value map.
+     */
+    private static HashMap<String, String> getTestbedParameters(String[] args) {
+        var parameters = new HashMap<String, String>();
+
+        for (int i = 0; i < args.length; i += 2) {
+            if (args[i].equalsIgnoreCase("-fmFile")) {
+                parameters.put("fmFile", args[i + 1]);
+            } else if (args[i].equalsIgnoreCase("-configurations")) {
+                parameters.put("configurations", args[i + 1]);
+            } else if (args[i].equalsIgnoreCase("-edgeIndex")) {
+                parameters.put("edgeIndex", args[i + 1]);
+            }
+        }
+
+        return parameters;
+    }
+
+
+    /**
      * Initializes services and launches the central manager.
      *
      * @param args command line arguments
      * @throws IOException if startup of network components fails
      */
-    public static void main(String[] args) throws IOException {
+    public static void main2(String[] args) throws IOException {
         var logParameters = new LogInformation(args);
         var logManager = new MyLogManager(logParameters);
         logManager.start();
