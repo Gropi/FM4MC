58907fc6ea4f4766b6dd915e06e7735aee7bc58f
Uwe Gropengießer <uwe@gropengiesser-online.de>
2025-08-21 08:36:26 +0200

    Removed not needed import

--- FM4MC/Configuration-Manager/src/test/java/GraphGenerator/GraphGeneratorV2Test.java
+++ FM4MC/Configuration-Manager/src/test/java/GraphGenerator/GraphGeneratorV2Test.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import ConfigurationCalculator.Structures.PartialConfiguration;
 import FeatureModelReader.Structures.Feature;
 import FeatureModelReader.Structures.FeatureConnectivityInformation;
-import GraphGenerator.GraphGeneratorV2;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
